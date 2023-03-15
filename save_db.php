a3a4406672779f90451c04c49d38fee94c1951d3
loveopo1999@gmail.com <loveopo1999@gmail.com>
2023-03-15 10:40:18 +0700

    update

--- save_db.php
+++ save_db.php
@@ -1,73 +1,78 @@
 <meta charset="utf-8">
 <?php
-
-if (isset($_POST['email']) && isset($_POST['poster_id'])) {
+if (isset($_POST['email'], $_POST['poster_id'])) {
   $email = $_POST['email'];
   $poster_id = $_POST['poster_id'];
 
-  // Check if both email and poster_id are not empty
-  if (!empty($email) && !empty($poster_id)) {
-    // Check if the email already exists in the database for this poster_id
-    $check_stmt = $conn->prepare("SELECT COUNT(*) FROM uesr WHERE email = ?");
+  if (!empty($email)) {
+    $check_stmt = $conn->prepare("SELECT COUNT(*) FROM user_reg WHERE email = ?");
     $check_stmt->execute([$email]);
     $num = $check_stmt->fetchColumn();
 
     if ($num > 0) {
-      // Display error message and redirect back to the form page
-      echo '<script>
-        swal({
-          title: "Eamil นี้ได้มีการโหวดคะแนนไปแล้ว",
-          type: "error"
-        }, function() {
-          window.location = "index.php";
-        });
-      </script>';
-      exit;
-    }
+      // Check if both email and poster_id are not empty
+      if (!empty($poster_id)) {
+        // Check if the email already exists in the database for this poster_id
+        $check_stmt = $conn->prepare("SELECT COUNT(*) FROM user WHERE email = ? AND poster_id = ?");
+        $check_stmt->execute([$email, $poster_id]);
+        $num = $check_stmt->fetchColumn();
 
-    // Insert the email and poster_id into the database
-    $insert_stmt = $conn->prepare("INSERT INTO uesr (email, poster_id) VALUES (?, ?)");
-    $result = $insert_stmt->execute([$email, $poster_id]);
+        if ($num > 0) {
+          // Display error message and redirect back to the form page
+          echo '<script>
+              swal({
+                title: "Email นี้ได้มีการโหวดคะแนนไปแล้ว",
+                text: "โหวตนวัตกรรมที่ท่านประทับใจได้เพียง 1 ผลงานที่เท่านั้น",
+                type: "error"
+              }, function() {
+                window.location = "index.php";
+              });
+            </script>';
+          exit;
+        }
 
-    if ($result) {
-      // Display success message and redirect to the next page
-      echo '<script>
-        swal({
-          title: "บันทึกคะแนนโหวดสำเร็จ",
-          text: "ขอบคุณนี้ลงคะแนน",
-          type: "success",
-          timer: 3000,
-          showConfirmButton: false
-        }, function(){
-          window.location.href = "index.php";
-        });
-      </script>';
-      exit;
+        // Insert the email and poster_id into the database
+        $insert_stmt = $conn->prepare("INSERT INTO user (email, poster_id) VALUES (?, ?)");
+        $insert_stmt->execute([$email, $poster_id]);
+
+        // Display success message and redirect to the next page
+        echo '<script>
+            swal({
+              title: "บันทึกคะแนนโหวดสำเร็จ",
+              text: "ขอบคุณที่ลงคะแนน",
+              type: "success",
+              timer: 3000,
+              showConfirmButton: false
+            }, function(){
+              window.location.href = "index.php";
+            });
+          </script>';
+        exit;
+      } else {
+        // Display error message and redirect back to the form page
+        echo '<script>
+            swal({
+              title: "กรุณากรอกข้อมูลให้ครบถ้วน",
+              type: "error"
+            }, function() {
+              window.location = "index.php";
+            });
+          </script>';
+        exit;
+      }
     } else {
       // Display error message and redirect back to the form page
       echo '<script>
-        swal({
-          title: "เกิดข้อผิดพลาด",
-          type: "error"
-        }, function() {
-          window.location = "index.php";
-        });
-      </script>';
-      exit;
-    }
-  } else {
-    // Display error message and redirect back to the form page
-    echo '<script>
-      swal({
-        title: "กรุณากรอกข้อมูลให้ครบถ้วน",
-        type: "error"
-      }, function() {
-        window.location = "index.php";
+          swal({
+            title: "Email ยังไม่ได้ทำการลงทะเบียน",
+            text: "กรุณาลงทะเบียนก่อนเข้างาน",
+            type: "error"
+          }, function() {
+            window.location = "index.php";
       });
     </script>';
-    exit;
+      exit;
+    }
   }
 }
-
-
 ?>
